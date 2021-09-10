e65d85585044fa6f709c86fa33df259e047fa665
Bassem Marji <74184047+bassemmarji@users.noreply.github.com>
2021-09-10 17:55:34 +0300

    Add files via upload

--- recognize_eyes_color.py
+++ recognize_eyes_color.py
@@ -1,21 +1,19 @@
 #Import Libraries
-import dlib,cv2,os,argparse,filetype
-from imutils import face_utils
-import webcolors
+import cv2,os,argparse,filetype
+import webcolors,imutils
 
-#Landmark's facial detector to estimate the location of 68 coordinates that map the facial points
-#in a person's face
-FACIAL_LANDMARK_PREDICTOR = '.\\static\\models\\shape_predictor_68_face_landmarks.dat'
+#The path to the pre-trained Haar cascade face detector
+HAAR_CASCADE_FACE_DETECTOR_PATH = '.\\static\\cascades\\haarcascade_frontalface_default.xml'
+#The path to the pre-trained Haar cascade eyes detector
+HAAR_CASCADE_EYES_DETECTOR_PATH = '.\\static\\cascades\\haarcascade_eye.xml'
 
-def initialize_dlib(facial_landmark_predictor:str):
+def initialize_cascade_detectors():
     """
-    Initialize dlib's face detetctor (HOG-based) and then create the facial landmark predictor
+    Initialize and the pre-trained Haar cascade detectors
     """
-    print('Loading facial landmark predictor...')
-    detector  = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor(FACIAL_LANDMARK_PREDICTOR)
-
-    return detector, predictor
+    face_cascade = cv2.CascadeClassifier(HAAR_CASCADE_FACE_DETECTOR_PATH)
+    eye_cascade = cv2.CascadeClassifier(HAAR_CASCADE_EYES_DETECTOR_PATH)
+    return face_cascade, eye_cascade
 
 def closest_color(req_color):
     """
@@ -49,106 +47,115 @@ def recognize_eyes_color(input_path:str):
     """
     Recognize the color of the eyes of the faces showing within the image
     """
-    #Initialize dlib face detector using the facial landmark recognition
-    detector, predictor = initialize_dlib(facial_landmark_predictor=FACIAL_LANDMARK_PREDICTOR)
+    #Initialize the Haar cascade face recognizer
+    face_cascade,eye_cascade = initialize_cascade_detectors()
 
     # Read Input Image
     img = cv2.imread(input_path)
 
+    # Initialize frame size
+    frame_width = 640
+    frame_height = 360
+
     # Preserve a copy of the original
     frame = img.copy()
+    frame = imutils.resize(frame, width=frame_width, height=frame_height)
 
     # Convert it to gray scale
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Detect faces in the gray scale frame
-    faces = detector(gray_frame, 0)
+    faces = face_cascade.detectMultiScale(
+                                          #Matrix of type CV_8U containing objects to be detected
+                                          image=gray_frame
+                                          #Specifying how much the image will be reduced at each scale
+                                        , scaleFactor=1.05
+                                          #Affect the quality of detected objects
+                                          #Higher values result in less detections but with higher quality
+                                        , minNeighbors=5
+                                          #Minimum possible object size. Objects smaller are ignored
+                                        ,minSize=(30,30)
+                                        ,flags=cv2.CASCADE_SCALE_IMAGE
+                                        )
 
-    #Left eye
-    (left_Start, left_End) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
-    #Right eye
-    (right_Start, right_End) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+    print("{} face(s) detected".format(len(faces)))
 
     # Loop over the faces detected
-    for idx, face in enumerate(faces):
-        print("Detection Face ID = {} - Position = Left:{} Top:{} Right:{} Botton:{}".format((idx+1), face.left(), face.top(), face.right(),
-                                                                       face.bottom()))
-        #Draw the face bounding box
-        (x,y,w,h) = face_utils.rect_to_bb(face)
-        #face_img = frame[y:y+h,x:x+w]
-
-        #Store the 2 eyes
-        eyes = []
+    for idx, (x,y,w,h) in enumerate(faces):
+        print("Detection Face ID = {}".format((idx+1)))
 
         #Draws blue box over the rectangle
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255,0,0),3)
 
-        # determine the facial landmarks for the face region, then
-        # convert the facial landmark (x, y) coordinates to a NumPy array
-        shape = predictor(gray_frame, face)
-        shape = face_utils.shape_to_np(shape)
+        #Region of ineterest in gray mode
+        roi_face_gray  = gray_frame[y:y+h,x:x+w]
+        # Region of ineterest in color mode
+        roi_face_color = frame[y:y+h,x:x+w]
 
-        # indexes for left eye key points
-        leftEye = shape[left_Start:left_End]
-        rightEye = shape[right_Start:right_End]
+        #Detecting eyes
+        eyes = eye_cascade.detectMultiScale(image=roi_face_gray
+                                          , scaleFactor = 1.3
+                                          # Affect the quality of detected objects
+                                          # Higher values result in less detections but with higher quality
+                                          , minNeighbors = 9
+                                          # Minimum possible object size. Objects smaller are ignored
+                                          , minSize = (30, 30))
 
-        # wrap in a list
-        eyes.append(leftEye)
-        eyes.append(rightEye)
+        print("{} eye(s) detected".format(len(eyes)))
+
+        roi_eye = None
         flag = 0
+        for idx, (x_eye, y_eye,eye_width,eye_height) in enumerate(eyes):
 
-        for index, eye in enumerate(eyes):
             flag += 1
             #Left edge of the eye
-            left_side_eye = eye[0]
+            left_side_eye = x_eye
             #Right edge of the eye
-            right_side_eye = eye[3]
+            right_side_eye = x_eye + eye_width
             #Top side of the eye
-            top_side_eye = eye[1]
+            top_side_eye = y_eye + eye_height
             #Bottom side of the eye
-            bottom_side_eye = eye[4]
-
-            # calculate height and width of dlib eye keypoints
-            eye_width = right_side_eye[0] - left_side_eye[0]
-            eye_height = bottom_side_eye[1] - top_side_eye[1]
+            bottom_side_eye = y_eye
 
             # create bounding box with buffer around keypoints
-            eye_x1 = int(left_side_eye[0]  - 0 * eye_width)
-            eye_x2 = int(right_side_eye[0] + 0 * eye_width)
-            eye_y1 = int(top_side_eye[1]   - 1 * eye_height)
-            eye_y2 = int(bottom_side_eye[1]+ 0.75  * eye_height)
+            eye_x1 = int(left_side_eye   - 0     * eye_width)
+            eye_x2 = int(right_side_eye  + 0.2   * eye_width)
+            eye_y1 = int(top_side_eye    - 0.75  * eye_height)
+            eye_y2 = int(bottom_side_eye + 0.75  * eye_height)
 
             # draw bounding box around eye roi
-            rect = cv2.rectangle(frame,(eye_x1, eye_y1), (eye_x2, eye_y2),(255,0,0),2)
+            rect = cv2.rectangle(roi_face_color,(eye_x1, eye_y1), (eye_x2, eye_y2),(255,0,0),2)
 
             # Get the desired eye region (RGB)
-            roi_eye = frame[eye_y1:eye_y2, eye_x1:eye_x2]
+            roi_eye = roi_face_color[eye_y1:eye_y2, eye_x1:eye_x2]
 
-            if flag==1:
+            if flag==2:
                break
 
-        x = roi_eye.shape
-        row = x[0]
-        col = x[1]
+        if roi_eye is not None:
+           x = roi_eye.shape
+           row = x[0]
+           col = x[1]
 
-        #Pick RGB values from the area just below pupil
-        arr_eye_color = roi_eye[row //2:(row //2) +1,((col //3) +3):((col //3)) + 6]
-        array1 = arr_eye_color[0][2]
+           #Pick RGB values from the area just below pupil
+           arr_eye_color = roi_eye[row //2:(row //2) +1,((col //3) +3):((col //3)) + 6]
 
-        # store it in tuple and pass this tuple to "find_color" Funtion
-        label = "Eyes color = {}".format( str(find_color(array1)).title())
+           array1 = arr_eye_color[0][2]
 
-        combined_img = cv2.hconcat([cv2.resize(frame        , (200, 200), cv2.INTER_CUBIC)
-                                   ,cv2.resize(arr_eye_color, (200, 200), cv2.INTER_CUBIC)])
-        if arr_eye_color is not None:
-           combined_img = cv2.hconcat([cv2.resize(frame        , (200, 200) , cv2.INTER_CUBIC)
-                                      ,cv2.resize(roi_eye      , (200, 200) , cv2.INTER_CUBIC)
-                                      ,cv2.resize(arr_eye_color, (200, 200) , cv2.INTER_CUBIC)])
+           #store it in tuple and pass this tuple to "find_color" Funtion
+           label = "Eyes color = {}".format( str(find_color(array1)).title())
 
-        # Display Image on screen
-        cv2.imshow(label,combined_img)
-        # Mantain output until user presses a key
-        cv2.waitKey(0)
+           combined_img = cv2.hconcat([cv2.resize(frame        , (200, 200), cv2.INTER_CUBIC)
+                                      ,cv2.resize(arr_eye_color, (200, 200), cv2.INTER_CUBIC)])
+           if arr_eye_color is not None:
+               combined_img = cv2.hconcat([cv2.resize(frame        , (200, 200) , cv2.INTER_CUBIC)
+                                          ,cv2.resize(roi_eye      , (200, 200) , cv2.INTER_CUBIC)
+                                          ,cv2.resize(arr_eye_color, (200, 200) , cv2.INTER_CUBIC)])
+
+           # Display Image on screen
+           cv2.imshow(label,combined_img)
+           # Mantain output until user presses a key
+           cv2.waitKey(0)
 
     # Cleanup
     cv2.destroyAllWindows()
